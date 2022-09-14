4881a1674014d7edce2fe2f1828c4cf694ac9b00
samuellaraagui <samuellara@iesmontsia.org>
2022-09-14 15:16:34 +0200

    helloWorld amb Python i métode Split()

---
+++ hello.py
@@ -0,0 +1,12 @@
+
+#Hello World en Python
+helloWorld = "Hola Mon Daw 2";
+
+#Hello World en Python amb el métode split()
+helloWorldSplit = helloWorld.split(); 
+
+#Impressió per pantalla de la variable helloWorld
+print(helloWorld);
+
+#Impressió per pantalla de la variable helloWorldSplit
+print(helloWorldSplit);
