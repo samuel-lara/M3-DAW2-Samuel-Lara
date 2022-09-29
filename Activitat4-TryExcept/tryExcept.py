d4d4bdc738aa0b16e05d195e7cab4a09b70f69ca
samuel-lara <samuellara@iesmontsia.org>
2022-09-29 21:03:45 +0200

    tryExcept funcionando en tryExcept pero al 50% en tryExceptObject

--- Activitat4-TryExcept/tryExcept.py
+++ Activitat4-TryExcept/tryExcept.py
@@ -1,18 +1,25 @@
+#creem una clase per a l'error personalitzat
+class ErrorPersonalitzat(Exception):
+    pass
+
+
+
 def notasAlumno():
     try:
         x = int(input("Afegir una nota: "))
         if x > 10:
-            raise OverflowError("ERROR - No es permet una nota major que 10") #error personalitzat
+            raise ErrorPersonalitzat("ERROR - No es permet una nota major que 10") #error personalitzat
         elif x < 0:
-            raise OverflowError("ERROR - No es permet una nota menor que 0") #error personalitzat
+            raise ErrorPersonalitzat("ERROR - No es permet una nota menor que 0") #error personalitzat
         
     except ValueError as e:  # Detecta si ha un error al tipus de dada
-        print("ERROR - S'ha d'introduïr un tipus de dada vàlid ", e)
+        print("ERROR - S'ha d'introduïr un tipus de dada vàlida: ", e)
     except KeyboardInterrupt as e:  # Detecta si s'ha desconectat el teclat o si l'usuari ha fet CTRL + C
         print("ERROR - S'ha tallat la connexió amb el teclat: ", e)
-    except OverflowError as e: #imprimim l'error personalitzat
+    except ErrorPersonalitzat as e: #imprimim l'error personalitzat
         print(e)
     finally:
         print("Aquesta línia sempre s'imprimeix")
 
-notasAlumno()
+if __name__ == "__main__":
+    notasAlumno()
---
+++ Activitat4-TryExcept/tryExceptObjectes.py
@@ -0,0 +1,37 @@
+'''Programa per a validar notes'''
+
+#Es crea la classe per a l'Error personalitzat
+class ErrorPersonalitzat(Exception):
+    pass
+
+class ValueError(Exception):
+    pass
+
+#Es crea la clase nota
+class Nota:
+    def __init__(self, nota): #constructor
+        self.nota = nota
+        
+        '''Solo me acepta los ERRORES que yo he creado'''
+        #Creamos una función para ejecutar el tryExcept
+    def notesAlumne(self, nota):
+        try:
+            if nota > 10:
+                raise ErrorPersonalitzat("ERROR - No es permet una nota major que 10") #error personalitzat
+            elif nota < 0:
+                raise ErrorPersonalitzat("ERROR - No es permet una nota menor que 0") #error personalitzat
+        except ErrorPersonalitzat as e: #imprimim l'error personalitzat - Funciona
+            print(e)
+        except ValueError as e:  # Detecta si ha un error al tipus de dada-No funciona
+            print("ERROR - S'ha d'introduïr un tipus de dada vàlid ", e)
+        except KeyboardInterrupt as e:  # Detecta si s'ha desconectat el teclat o si l'usuari ha fet CTRL + C - No funciona
+            print("ERROR - S'ha tallat la connexió amb el teclat: ", e)
+        finally:
+            print("Aquesta línia sempre s'imprimeix")
+
+if __name__ == "__main__":
+    #creem l'objecte nota
+    notaIntroduida = int(input("Afegir una nota: "))
+    Nota1 = Nota(notaIntroduida)
+    print(Nota1.nota)
+    Nota1.notesAlumne(Nota1.nota)
